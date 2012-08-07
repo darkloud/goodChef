6378d07589d8e53e12031c495c2576f75d607cce
maxifeng <darkloud@126.com>
2012-08-07 14:53:57 +0800

    start

---
+++ GoodChef.site/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ToolsDll;
+
+namespace GoodChef.site
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            parseHTMLContent pc = new parseHTMLContent();
+            pc.loadFromFile();
+        }
+    }
+}
---
+++ GoodChef.site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoodChef.site.Global" Language="C#" %>
